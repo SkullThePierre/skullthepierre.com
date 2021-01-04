ebe5f56f5b0ceff3158d7baa88705a78c2366047
ParthThakur <parththakur@gmail.com>
2021-01-04 23:11:02 +0000

    Add background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'pierre-tall': "url('/img/background.jpg')",
+      })
+    },
   },
   variants: {
     extend: {},
