a9fe4fd12b6a13625e1528e6bc250b33fedece0e
ParthThakur <parththakur@gmail.com>
2021-01-11 17:35:49 +0000

    Detect when central logo is at the centre of the screen.

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,20 @@ let currentYear = (new Date).getFullYear();
 $(document).ready(function () {
     $('#currentYear').text(currentYear);
 })
+
+//Detect when skp-centre-logo is in the middle of the screen.
+let skpLogo = $('#skp-logo');
+let timeout = null;
+
+$(window).scroll(function () {
+    if (!timeout) {
+        timeout = setTimeout(function () {
+            clearTimeout(timeout);
+            timeout = null;
+            if (skpLogo[0].getBoundingClientRect().y + skpLogo.height()/2 <= window.innerHeight/2){
+                replaceLogo();
+            }
+
+        }, 250);
+    }
+});
