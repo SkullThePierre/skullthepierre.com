0bf9373476c25f999520cb11360422a4c601691e
ParthThakur <parththakur@gmail.com>
2021-03-15 20:24:25 +0000

    Fix meta title

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,17 @@
+$.fn.exists = function () {
+    return this.length !== 0;
+}
+
 //Set copyright year to current year automatically.
 let currentYear = (new Date).getFullYear();
+let titleText = $('#title-text');
+let metaTitle = $('meta[name=title-text]');
+
 $(document).ready(function () {
     $('#currentYear').text(currentYear);
-    $('#title-text').text($('meta[name=title-text]').attr('content'))
+
+    if (metaTitle.exists()) {
+        titleText.text(metaTitle.attr('content'))
+        titleText.removeClass("hidden")
+    }
 })
