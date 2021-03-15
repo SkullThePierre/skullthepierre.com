920eec1c538ec1354d6d78ebca9e8ac4045d0aea
ParthThakur <parththakur@gmail.com>
2021-03-15 20:03:23 +0000

    Add meta title

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,5 @@
 let currentYear = (new Date).getFullYear();
 $(document).ready(function () {
     $('#currentYear').text(currentYear);
+    $('#title-text').text($('meta[name=title-text]').attr('content'))
 })
