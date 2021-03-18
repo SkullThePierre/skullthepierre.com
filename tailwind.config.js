5cf51673d27458b64bb7bb6d8d26e669b9974c0b
ParthThakur <parththakur@gmail.com>
2021-03-18 18:18:26 +0000

    aesthetic changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: theme => ({
+      backgroundImage: {
         'pierre-tall': "url('/img/bgs/background.jpg')",
         'contact-page-wide': "url('/img/bgs/bg-contact-wide.jpg')",
         'contact-page-tall': "url('/img/bgs/bg-contact-tall.jpg')",
         'contact-card': "url('/img/bgs/bg-contact-card.jpg')",
         'contact-card-b': "url('/img/bgs/bg-contact-card-bottom.png')",
         'under-construction': "url('/img/bgs/bg-under-construction.jpg')"
-      }),
+      },
 
       screens: {
         'lg': '1080px', // re-define screen-lg to 1080px.
