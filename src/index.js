f4dd63d8b6f776555cac8bf77fcec826130e925b
ParthThakur <parththakur@gmail.com>
2021-05-04 17:39:07 +0100

    Add functions to webpack

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import Loadchanges from "./js/load-changes";
+import replaceLogo from "./js/replace-logo";
+import submitForm from "./js/submit-form";
 import main from "./js/main";
+
+const homeRegex = new RegExp(".com/$", "i");
+const contactRegex = new RegExp("contact", "i");
 
 Loadchanges();
 
 $(document).ajaxStop(function () {
   main();
+
+  if (homeRegex.test($(location).attr("href"))) {
+    replaceLogo();
+  }
+
+  if (contactRegex.test($(location).attr("href"))) {
+    submitForm();
+  }
 });
