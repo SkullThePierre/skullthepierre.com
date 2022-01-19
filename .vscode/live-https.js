82c81c97b756dbf3cc51e13583b4a6568c36db32
ParthThakur <parththakur@gmail.com>
2022-01-19 19:08:40 +0000

    files for live-server https

---
+++ .vscode/live-https.js
@@ -0,0 +1,7 @@
+var fs = require("fs");
+
+module.exports = {
+  cert: fs.readFileSync(__dirname + "/server.crt", "utf8"),
+  key: fs.readFileSync(__dirname + "/server.key", "utf8"),
+  passphrase: "12345",
+};
